34a83365609be313c519d2f678b28f032157cf02
chenwu.fcw <chenwu.fcw@alibaba-inc.com>
2018-05-29 17:16:56 +0800

    Updates

--- examples/base.js
+++ examples/base.js
@@ -95,7 +95,7 @@ var App = function (_React$Component) {
               return document.body;
             } } : {}, {
             className: 'forTest',
-            direction: 'up',
+            direction: 'down',
             refreshing: this.state.refreshing,
             onRefresh: function onRefresh() {
               _this2.setState({ refreshing: true });
@@ -104,7 +104,7 @@ var App = function (_React$Component) {
               }, 1000);
             },
             indicator: { deactivate: '下拉' },
-            damping: 60
+            damping: 150
           }),
           [1, 2, 3, 4, 5, 6, 7].map(function (i) {
             return __WEBPACK_IMPORTED_MODULE_7_react___default.a.createElement(
